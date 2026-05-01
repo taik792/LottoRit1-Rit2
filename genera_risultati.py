5d4b5cca8fdcb2aa1b537e647ef696cc6764fd54
taik792 <rosario79@libero.it>
2026-05-01 10:10:16 +0200

    Update genera_risultati.py

--- genera_risultati.py
+++ genera_risultati.py
@@ -14,7 +14,7 @@ RUOTE = [
     "Nazionale"
 ]
 
-NUMERO_ESTRAZIONI = 200  # qui il motore usa storico lungo
+NUMERO_ESTRAZIONI = 500  # qui il motore usa storico lungo
 
 
 def carica_estrazioni():
